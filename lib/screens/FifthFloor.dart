919664d769628b9c2218b535f239df6cb1a4634f
chaeyeonhan1225 <gkscodus11@gmail.com>
2020-08-23 17:53:38 +0900

    학식페이지 CupertinoActivicyIndicatorbar로 수정

--- lib/screens/FifthFloor.dart
+++ lib/screens/FifthFloor.dart
@@ -74,7 +74,7 @@ class _FifthFloorState extends State<FifthFloor> {
                     Text(
                       "학식 정보",
                       style: TextStyle(
-                        color:  Color(0xffeb577c),
+                        color: Color(0xffeb577c),
                         fontWeight: FontWeight.w300,
                         fontFamily: "NotoSansKR",
                         fontStyle: FontStyle.normal,
@@ -298,7 +298,9 @@ class _FifthFloorState extends State<FifthFloor> {
                 Center(
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: CircularProgressIndicator(),
+                    child: CupertinoActivityIndicator(
+                      radius: 20,
+                    ),
                   ),
                 ),
               ],
